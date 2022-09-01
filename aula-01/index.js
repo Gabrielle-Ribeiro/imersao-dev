aae1b03120eb8c11496544b8fc4f4f4f396b3c62
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2022-09-01 15:57:51 -0300

    Adiciona projeto de calculadora da aula 01

---
+++ aula-01/index.js
@@ -0,0 +1,28 @@
+// ************** AULA 01 **************
+// Tópicos da Primeira Aula:
+// variáveis, strings, console.log, toFixed, operações matemáticas, concatenação.
+
+var nome = "Guilherme"
+var notaPrimeiroBimestre = 9
+var notaSegundoBimestre = 7
+var notaTerceiroBimestre = 4
+var notaQuartoBimestre = 2
+
+var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4
+
+var notaFizada = notaFinal.toFixed(1)
+
+console.log("Bem-vindo" + nome)
+console.log(notaFinal)
+
+// ************** DESAFIOS **************
+
+// 1) Quebrar a soma e divisão e mostrar se a pessoa foi aprovada
+
+// 2) Estilizar a página trocando cor de fundo e imagem
+
+// 3) Escrever na página o que escrevemos no console
+
+// 4) Criar conversor de temperaturas/moeda
+
+// 5) Imprimir a média usando as operações diretamente no console.log
