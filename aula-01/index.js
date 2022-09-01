d69d9583f8c42c462091c4ca4db90ae83207c324
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2022-09-01 18:07:46 -0300

    Adiciona desafios da aula 01

--- aula-01/index.js
+++ aula-01/index.js
@@ -10,19 +10,34 @@ var notaQuartoBimestre = 2
 
 var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4
 
-var notaFizada = notaFinal.toFixed(1)
+var notaFixada = notaFinal.toFixed(1)
 
-console.log("Bem-vindo" + nome)
-console.log(notaFinal)
+console.log("Bem-vindo " + nome)
+console.log(notaFixada)
 
 // ************** DESAFIOS **************
 
 // 1) Quebrar a soma e divisão e mostrar se a pessoa foi aprovada
 
+var somaNotaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre)
+var novaNotaFinal = somaNotaFinal / 4
+
+console.log("A sua nota final foi " + novaNotaFinal)
+
 // 2) Estilizar a página trocando cor de fundo e imagem
+// (Está nos outros códigos)
 
 // 3) Escrever na página o que escrevemos no console
 
+var elementoMensagem = document.getElementById("mensagem")
+elementoMensagem.innerHTML = "Sua nota final é " + notaFixada
+
 // 4) Criar conversor de temperaturas/moeda
 
+var temperaturaCelsius = 30
+var temperaturaFahrenheit = (temperaturaCelsius * (9/5)) + 32
+console.log("A temperatura " + temperaturaCelsius + "°C em Fahrenheit é " + temperaturaFahrenheit + "F.")
+
 // 5) Imprimir a média usando as operações diretamente no console.log
+
+console.log("A média é " + (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4)
