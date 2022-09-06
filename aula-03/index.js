b556bc90606a4dd7002ae153d3a594eca4772c3b
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2022-09-05 22:22:45 -0300

    Adiciona desafios da aula 03

--- aula-03/index.js
+++ aula-03/index.js
@@ -1,19 +1,43 @@
 // ************** AULA 03 **************
 
 var numeroSecreto = parseInt(Math.random() * 11)
+var quantidadeTentativas = 3
+var tentativa = 1
 
 function Chutar() {
   var elementoResultado = document.getElementById("resultado")
+  var elementoDica = document.getElementById("dica")
   var chute = parseInt(document.getElementById("valor").value)
+  var acertou = false
 
-  if (chute == numeroSecreto) {
-    elementoResultado.innerHTML = "Você acertou!"
-    console.log("acertou")
-  } else if (chute > 10 || chute < 0) {
-    elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10"
+  if(tentativa <= quantidadeTentativas){  
+    if (chute == numeroSecreto) {
+      elementoResultado.innerHTML = "Tentativa " + tentativa + "/" + quantidadeTentativas + "\nVocê acertou!"
+      elementoDica.innerHTML = ""
+      tentativa = quantidadeTentativas + 1
+      acertou = true
+    } else if (chute > 10 || chute < 0) {
+      elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10"
+      elementoDica.innerHTML = ""
+    } else {
+      elementoResultado.innerHTML = "Tentativa " + tentativa + "/" + quantidadeTentativas + "\nVocê errou!!"
+      tentativa++
+      if(chute < numeroSecreto) {
+        elementoDica.innerHTML = "Seu chute foi menor que o número secreto"
+      } else {
+        elementoDica.innerHTML = "Seu chute foi maior que o número secreto"
+      }
+    }
   } else {
-    elementoResultado.innerHTML = "Você errou!!"
+    if (acertou) {
+      elementoResultado.innerHTML = "Você já acertou. O número secreto era " + numeroSecreto
+      elementoDica.innerHTML = ""
+    } else {
+      elementoResultado.innerHTML = "Você não possui mais tentativas. O número secreto era " + numeroSecreto
+      elementoDica.innerHTML = ""
+    }
   }
+
 }
 
 // ************** DESAFIOS **************
@@ -23,3 +47,11 @@ function Chutar() {
 // 2) Quando a pessoa errar, dar dica de se o número secreto é maior ou menor do que o chute
   
 // 3) Pesquisar sobre a diferença entre == e ===
+// O == compara somente o conteúdo entre duas variáveis. Desse modo, a comparação a seguir retorna true.
+// var num = 5
+// var numString = "5"
+// var comparacao = num == numString
+// Já o === compara o conteúdo e o tipo entre duas variáveis. Desse modo, a comparação a seguir retorna false.
+// var num = 5
+// var numString = "5"
+// var comparacao = num === numString
