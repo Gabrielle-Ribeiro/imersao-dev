80475b96522e528d221ac9e82f850485053efe2d
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2022-09-14 19:12:20 -0300

    Adiciona aula 04

---
+++ aula-04/app.js
@@ -0,0 +1,20 @@
+// ************** AULA 04 **************
+var listaFilmes = [
+    "https://upload.wikimedia.org/wikipedia/pt/7/79/Yesterday_%282019%29_poster.jpg",
+    "https://1.bp.blogspot.com/-ImZPRqLsluE/WFK156_6pNI/AAAAAAAAYBY/0lEhNRF5wfQdLfr6hpT57_Jt2eBrE9H5wCLcB/s1600/arrival-kartoun-desert.jpg",
+    "https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg"
+  ];
+  
+  for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">");
+  }
+  
+  // ************** DESAFIOS **************
+  
+  // 1 - Fazer catálogo de outras coisas
+  
+  // 2 - Percorrer a lista por foreach e while
+  
+  // 3 - Não permitir adicionar filmes repetidos
+  
+  // 4 - Inserir a imagem pela tela
